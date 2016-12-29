5a4db7edaa77621ea0200dd01d435e8180820724
bigchou <40347905S@gmail.com>
2016-12-29 20:39:10 +0800

    modify_Search01

--- NTNU Image Talk Mobile/ImageTableViewController.swift
+++ NTNU Image Talk Mobile/ImageTableViewController.swift
@@ -13,19 +13,23 @@ class ImageTableViewController: UITableViewController, UISearchResultsUpdating, 
     @IBAction func unwindToHomeScreen(segue:UIStoryboardSegue){
     
     }
+    /*
     var myimages:[MyImage] = [
         MyImage(description:"Cafe Deadend", image:"Hamburger"),
         MyImage(description:"Homei", image:"Hamburger2"),
         MyImage(description:"Traif", image:"Hamburger3")
-    ]
+    ]*/
+    
+    
     var filteredmyimages = [MyImage]()
     var shouldShowSearchResults = false
+    var shouldUpdateSearchResults = false
     var searchController: UISearchController!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad")
+        //print("viewDidLoad")
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -41,7 +45,7 @@ class ImageTableViewController: UITableViewController, UISearchResultsUpdating, 
     
     
     func configureSearchController() {
-        print("configureSearchController")
+        //print("configureSearchController")
         // Initialize and perform a minimum configuration to the search controller.
         searchController = UISearchController(searchResultsController: nil)
         searchController.searchResultsUpdater = self
@@ -49,69 +53,104 @@ class ImageTableViewController: UITableViewController, UISearchResultsUpdating, 
         searchController.searchBar.placeholder = "Search here..."
         searchController.searchBar.delegate = self
         self.definesPresentationContext = true
+        
         searchController.searchBar.sizeToFit()
         // Place the search bar view to the tableview headerview.
         self.tableView.tableHeaderView = searchController.searchBar
     }
     
+    
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        if(filteredmyimages.count != 0){
+            filteredmyimages.removeAll()
+        }
         print("Begin Editing")
-        shouldShowSearchResults = false
-        self.tableView.reloadData()
+        shouldUpdateSearchResults = false
+        //shouldShowSearchResults = false
+        //self.tableView.reloadData()
     }
     
     
+    
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        /*
+        if(filteredmyimages.count != 0){
+            filteredmyimages.removeAll()
+        }*/
         print("Cancel!")
-        shouldShowSearchResults = false
-        self.tableView.reloadData()
+        shouldUpdateSearchResults = false
+        //shouldShowSearchResults = false
+        tableView.reloadData()
     }
     
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        /*
+        let tmp = ["a1","b2","c3","d4"]
+        for item in tmp{
+            let tmpobj = MyImage(description:item,image:"Hamburger")
+            filteredmyimages.append(tmpobj)
+        }*/
+        
+        shouldUpdateSearchResults = true;
+        
+        
+        
         print("Search is Pressed")
         shouldShowSearchResults = true
-        self.tableView.reloadData()
-        //searchController.searchBar.resignFirstResponder() // close keyboard
+        updateSearchResults(for: searchController)
+        
+        searchController.searchBar.resignFirstResponder() // close keyboard
+        
+        
+        
+        
     }
     
     
 
     
     override func viewWillAppear(_ animated: Bool){
-        print("viewWillAppear")
+        //print("viewWillAppear")
         super.viewWillAppear(animated)
         navigationController?.hidesBarsOnTap = false
     }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        print("numberOfSections")
+        //print("numberOfSections")
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("numberOfRowsInSection")
+        //print("numberOfRowsInSection")
         // #warning Incomplete implementation, return the number of rows
+        return filteredmyimages.count
+        /*
         if(shouldShowSearchResults){
+            //print("count"+String(filteredmyimages.count))
             return filteredmyimages.count
         }
         else {
+            //print("count"+String(myimages.count))
             return myimages.count
-        }
+        }*/
     }
 
     
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("cellForRowAt")
+        //print("cellForRowAt")
         let cellIdentifier = "Cell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! ImageTableViewCell
         // Configure the cell...
         cell.thumbnailImageView.contentMode = UIViewContentMode.scaleAspectFill
+        cell.imageDescription.text = filteredmyimages[indexPath.row].description
+        cell.thumbnailImageView.image = UIImage(named:filteredmyimages[indexPath.row].image)
+        /*
         if(shouldShowSearchResults){
             cell.imageDescription.text = filteredmyimages[indexPath.row].description
             cell.thumbnailImageView.image = UIImage(named:filteredmyimages[indexPath.row].image)
@@ -119,25 +158,46 @@ class ImageTableViewController: UITableViewController, UISearchResultsUpdating, 
         else {
             cell.imageDescription.text = myimages[indexPath.row].description
             cell.thumbnailImageView.image = UIImage(named:myimages[indexPath.row].image)
-        }
+        }*/
         return cell
     }
 
     
     
-    
+    /*
     func filterContentForSearchText(searchText: String){
         filteredmyimages = myimages.filter({ (myimage:MyImage) -> Bool in
             let nameMatch = myimage.description.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
             return nameMatch != nil
         })
     }
+    */
     func updateSearchResults(for searchController: UISearchController) {
-        print("updateSearchResults")
+        print("update")
+        /*
+        var tmp = ["a1","b2","c3","d4"]
+        for item in tmp{
+            var tmpobj = MyImage(description:item,image:"Hamburger")
+            filteredmyimages.append(tmpobj)
+        }*/
+        
+        
+        
+        /*
+        print("updateSearchResults: "+searchController.searchBar.text!)
         if let searchText = searchController.searchBar.text{
             filterContentForSearchText(searchText: searchText)
             tableView.reloadData()
+        }*/
+        if(shouldUpdateSearchResults == true){
+            print("Updating")
+            for random in 1...30 {
+                //let random = arc4random_uniform(20) + 10; // range between 10 and 30
+                let tmpobj = MyImage(description: String(random), image: "Hamburger")
+                filteredmyimages.append(tmpobj)
+            }
         }
+        tableView.reloadData()
     }
  
  
@@ -181,7 +241,7 @@ class ImageTableViewController: UITableViewController, UISearchResultsUpdating, 
     
     
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        print("swipe button")
+        //print("swipe button")
         let share = UITableViewRowAction(style: .default, title: "Share") {
             (action, indexPath) in
             // share item at indexPath
@@ -197,23 +257,26 @@ class ImageTableViewController: UITableViewController, UISearchResultsUpdating, 
     
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print("prepareforsegue")
+        //print("prepareforsegue")
         if segue.identifier == "showImageDetail"{
             if let indexPath = tableView.indexPathForSelectedRow{
                 let destinationController = segue.destination as! imageDetailViewController
+                destinationController.imageImage = filteredmyimages[indexPath.row].image
+                destinationController.imageName = filteredmyimages[indexPath.row].description
+                /*
                 if(shouldShowSearchResults){
                     destinationController.imageImage = filteredmyimages[indexPath.row].image
                     destinationController.imageName = filteredmyimages[indexPath.row].description
                 }else{
                     destinationController.imageImage = myimages[indexPath.row].image
                     destinationController.imageName = myimages[indexPath.row].description
-                }
+                }*/
             }
         }
     }
     
     override func didReceiveMemoryWarning() {
-        print("didReceiveMemoryWarning")
+        //print("didReceiveMemoryWarning")
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
